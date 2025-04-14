a3a66aceb7ccd34e7c106a89deefffcbaa41a10c
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-14 05:29:32 +0200

    a

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { userById } from "@/lib/user.actions";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
-export default function page() {
+export default async function Page({
+
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const user = await userById(parseInt(id));
+
+  if (!user) {
+    notFound();
+  }
+
   return (
-    <div>page</div>
-  )
+    <div>
+
+
+    <li key={user.id}>
+        {user.name} - {user.email} - {user.age}
+        <Link href={`/user/${user.id}`}>View</Link>
+        <Link href={`/user/${user.id}/edit`}>Edit</Link>
+        <Link href={`/user/${user.id}/delete`}>Delete</Link>
+    </li>
+    </div>
+  );
 }
+
+
