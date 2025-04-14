5c36a31b420fa1decc4a258b685c1efd26c4f08f
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-14 23:08:43 +0200

    aaaa

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
 
-import prisma from "@/lib/prisma";
-import Link from "next/link";
+
 import Gost from "./gost/page";
 
 export default async function Home() {
 
-  // const gosts= await prisma.gost.findMany()
+
   return (
   <>
-{/* {gosts.map((gost) => (
-    <div key={gost.id}>
-      <h2>{gost.name}</h2>
-      <p>{gost.email}</p>
-      <p>{gost.age}</p>
-    </div>
-  ))} */}
 
 
 <Gost/>
