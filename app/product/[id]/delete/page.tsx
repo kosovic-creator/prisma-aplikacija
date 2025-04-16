1dae24f2d0e26189f76be027e8f438f3fa4e9d6f
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-16 05:54:15 +0200

    delete

--- app/product/[id]/delete/page.tsx
+++ app/product/[id]/delete/page.tsx
@@ -15,21 +15,11 @@ export default async function page({ params }: PageProps) {
   }
     return (
       <>
-        <div className="flex flex-col items-center justify-center min-h-screen">
-          <h1 className="text-3xl font-bold">Delete Product</h1>
-          <p className="text-lg">Are you sure you want to delete this product?</p>
-          <div className="flex gap-4">
-            <Link href="/product/" className="btn btn-primary">
-              Yes
-            </Link>
-            <Link href="/product" className="btn btn-secondary">
-              No
-            </Link>
-          </div>
-          <div className="flex gap-4">
-            <Link href="/product" className="btn btn-primary">Go Back</Link>
-          </div>
-        </div>
+        <h1>Delete Product</h1>
+        <p>Product with ID {id} has been deleted.</p>
+        <Link href="/product">Go back to product list</Link>
+        <Link href={`/product/${id}/update`}>Update Product</Link>
+        <Link href={`/product/${id}/delete`}>Delete Product</Link>
       </>
     );
   }
